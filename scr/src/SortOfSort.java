9c8e9b95d16bdd67e39b905b87ca9f53e86bd96a
sergioavalos <geo.avalos1@gmail.com>
2020-03-02 14:47:19 -0700

    Started project

---
+++ scr/src/SortOfSort.java
@@ -0,0 +1,13 @@
+public class SortOfSort {
+
+    public static void sortOfSort(int [] set){
+        int count = 0;
+        int place = 0;
+        for(int i =0;i <set.length-1 ; i++){
+            count++;
+            for(int j = 0;j <set.length-1; i++;)
+
+            //if()
+        }
+    }
+}
