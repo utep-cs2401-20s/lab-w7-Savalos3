58a8822729f92d79a78e484749c8e37a01f278bc
sergioavalos <geo.avalos1@gmail.com>
2020-03-06 14:12:32 -0700

    finshed code , just need to add comments on tester.

--- scr/src/SortOfSort.java
+++ scr/src/SortOfSort.java
@@ -1,13 +1,47 @@
 public class SortOfSort {
 
-    public static void sortOfSort(int [] set){
-        int count = 0;
-        int place = 0;
-        for(int i =0;i <set.length-1 ; i++){
-            count++;
-            for(int j = 0;j <set.length-1; i++;)
+    public static int[] sortOfSort(int [] set){
+        int n = set.length;
+        int left = 0; //controls where to start search when going through array.
+        int right = set.length-1; //controls where to end search when going through array
 
-            //if()
-        }
+        for(int i = set.length-1; i >= 0; i--){
+
+            int max = right;
+            if(i%n == n-1  || i % n == n-2 )  {
+                for (int j = right - 1; j >= left; j--) {
+                    if (set[j] > set[max]) {
+                        max = j;
+                    }
+                }
+                int temp = set[max];
+                set[max] = set[right];
+                set[right] = temp;
+                right--;
+                if(left == right ){ //ends when the ends meet.
+                    return set;
+                }
+            }else{
+                max = left;
+                  for (int j = left + 1; j < right+1; j++) {
+                      if (set[j] > set[max]) {
+                          max = j;
+                      }
+                  }
+            if(i % n == n-4 ){ //This in a way changes the way the loops see the length of the array.
+                n = n - 4; // n is used to make sure the conditionals are met no matter the length of the initial array.
+            }
+                int temp = set[max];
+                set[max] = set[left];
+                set[left] = temp;
+                left++;
+                if(left == right){ //ends when the ends meet.
+                    return set;
+                }
+            }
+
+            }
+        return set;
+
     }
 }
--- scr/src/SortOfSortTester.java
+++ scr/src/SortOfSortTester.java
@@ -1,2 +1,101 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class SortOfSortTester {
+    @Test
+    public void SortofSort1(){
+        SortOfSort test = new SortOfSort();
+
+        int [] example = {3,4};
+        int [] ex = {3,4};
+        int [] result = test.sortOfSort(example);
+
+        assertArrayEquals(ex , example);
+    }
+    @Test
+    public void SortofSort2(){
+        SortOfSort test = new SortOfSort();
+
+        int [] example = {-4,2,1,5,-6};
+        int [] ex = {1,-4,-6,2,5};
+        int [] result = test.sortOfSort(example);
+
+        assertArrayEquals(ex , example);
+    }
+    @Test
+    public void SortofSort3(){
+        SortOfSort test = new SortOfSort();
+
+        int [] example = {3,4,2};
+        int [] ex = {2,3,4};
+        int [] result = test.sortOfSort(example);
+
+        assertArrayEquals(ex , example);
+    }
+    @Test
+    public void SortofSort4(){
+        SortOfSort test = new SortOfSort();
+
+        int [] example = {3,4,2,1};
+        int [] ex = {2,1,3,4};
+        int [] result = test.sortOfSort(example);
+
+        assertArrayEquals(ex , example);
+        }
+    @Test
+    public void SortofSort5(){
+        SortOfSort test = new SortOfSort();
+
+        int [] example = {4,3,2,1};
+        int [] ex = {2,1,3,4};
+        int [] result = test.sortOfSort(example);
+
+        assertArrayEquals(ex , example);
+    }
+
+    @Test
+    public void SortofSort6(){
+        SortOfSort test = new SortOfSort();
+
+        int [] example = {3,4,5,1,2};
+        int [] ex = {3,2,1,4,5};
+        int [] result = test.sortOfSort(example);
+
+        assertArrayEquals(ex , example);
+    }
+
+    @Test
+    public void SortofSort7(){
+        SortOfSort test = new SortOfSort();
+
+        int [] example = {7,4,5,1,6,3,2};
+        int [] ex = {5,4,1,2,3,6,7};
+        int [] result = test.sortOfSort(example);
+
+        assertArrayEquals(ex , example);
+    }
+    @Test
+    public void SortofSort8(){
+        SortOfSort test = new SortOfSort();
+
+        int [] example = {7,4,5,9,6,3,2,8,1};
+        int [] ex = {7,6,3,2,1,4,5,8,9};
+        int [] result = test.sortOfSort(example);
+
+        assertArrayEquals(ex , example);
+    }
+    @Test
+    public void SortofSort9(){
+        SortOfSort test = new SortOfSort();
+
+        int [] example = {10,9,8,7,6,5,4,3,2,1};
+        int [] ex = {8,7,4,3,1,2,5,6,9,10};
+        int [] result = test.sortOfSort(example);
+
+        assertArrayEquals(ex , example);
+    }
 }
